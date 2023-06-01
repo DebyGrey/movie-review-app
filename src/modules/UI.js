b7a06942622f542a525f9337c772e9e5a10b9f38
DebyGrey <demifashoro@gmail.com>
2023-06-01 19:04:27 +0100

    feature add new like

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,4 +1,5 @@
 import API from './API.js';
+
 // Display movies
 export default class UI {
   static displayMovies = async () => {
@@ -12,19 +13,32 @@ export default class UI {
       movieList.forEach((movie) => {
         const movieListItem = document.createElement('li');
         movieListItem.classList.add('movie-list-item');
-        const movieListItemContent = `<img src= "${movie.image.medium}" class= ""/> <div class="movie-title-header"><h4>${movie.name}</h4> <i id="${movie.id}" class="fa-regular fa-heart fa-xl" aria-hidden="true"></i></div><p class="likes-counter-container"><span class="likes-counter"></span> likes</p><button class="btn" id="comments-btn" type="buttton">Comments</button><button class="btn" id="reservation-btn" type="button">Reservations</button>`;
+        const movieListItemContent = `<img src= "${movie.image.medium}"/> <div class="movie-title-header"><h4>${movie.name}</h4> <button type="button" class="like-btn"><i id="${movie.id}" class="fa-regular fa-heart fa-xl" aria-hidden="true"></i></button></div><p class="likes-counter-container"><span class="likes-counter"></span> likes</p><button class="btn" id="comments-btn" type="buttton">Comments</button><button class="btn" id="reservation-btn" type="button">Reservations</button>`;
         movieListItem.innerHTML = movieListItemContent;
         movieListContainer.appendChild(movieListItem);
 
         // Update likes counter
         const likesCounter = movieListItem.querySelector('.likes-counter');
-        const movieLikes = likesCount.find((item) => item.item_id === movie.id);
+        const movieLikes = likesCount.find((item) => item.item_id === JSON.stringify(movie.id));
+
         if (movieLikes) {
           likesCounter.textContent = movieLikes.likes;
         } else {
           likesCounter.textContent = '0';
         }
+
+        // Add event listener to like-btn
+        const likeBtn = movieListItem.querySelector('.like-btn');
+        likeBtn.addEventListener('click', UI.addLikes);
       });
     }
   };
+
+  // add likes
+  static addLikes = async (e) => {
+    e.preventDefault();
+    const likeID = e.target.id;
+    await API.postLikes(likeID);
+    UI.displayMovies();
+  }
 }
