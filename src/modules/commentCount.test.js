bfe71b43f7c7c00440b841160cbb2e840af5cb7c
David Katende <katendedavidjoga@gmail.com>
2023-06-02 17:59:29 +0300

    Fix mesage conter

--- src/modules/commentCount.test.js
+++ src/modules/commentCount.test.js
@@ -9,8 +9,8 @@ describe('Comment', () => {
 
   it('should return the correct number of comments for an item', async () => {
     const comments = [
-      { name: 'David', comment: 'This is a comment 1' },
-      { name: 'Derby', comment: 'This is a comment 2' },
+      { username: 'David', comment: 'This is a comment 1' },
+      { username: 'Derby', comment: 'This is a comment 2' },
     ];
 
     jest.spyOn(Comment, 'getComments').mockResolvedValue(comments);
@@ -22,9 +22,9 @@ describe('Comment', () => {
 
     await Comment.displayComments(itemId);
 
-    expect(container.innerHTML).toContain(`<b>Comments</b> <b>(</b>${comments.length}<b>)</b>`);
-    expect(container.innerHTML).toContain(`${comments[0].name}: ${comments[0].comment}`);
-    expect(container.innerHTML).toContain(`${comments[1].name}: ${comments[1].comment}`);
+    expect(container.innerHTML).toContain(`<b><h5>Comments</h5> (${comments.length})</b>`);
+    expect(container.innerHTML).toContain(`${comments[0].username}: ${comments[0].comment}`);
+    expect(container.innerHTML).toContain(`${comments[1].username}: ${comments[1].comment}`);
 
     expect(Comment.getComments).toHaveBeenCalledWith(itemId);
   });
