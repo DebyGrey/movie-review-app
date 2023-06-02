05fa132bd0d1f89183dcf319cd97e02460d48542
DebyGrey <demifashoro@gmail.com>
2023-06-02 06:52:05 +0100

    implements unit testing for like counter and refactor like update to laod faster

--- src/modules/API.js
+++ src/modules/API.js
@@ -1,8 +1,5 @@
 export default class API {
-  // APP ID
   static appID = 'KBe5C9DIOBKtmbfBe81E';
-
-  // static testAppID = '3wtYjl1ATbHwpm26nJ9e';
 
   static baseURL = 'https://api.tvmaze.com';
 
@@ -10,23 +7,25 @@ export default class API {
 
   static url = API.baseURL + API.showMoviesURL;
 
-  // base url and endpoints for involvement API
   static appBaseURL =
     'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
 
-  static likesURL = `${API.appBaseURL}apps/3wtYjl1ATbHwpm26nJ9e/likes`;
+  static likesURL = `${API.appBaseURL}apps/KBe5C9DIOBKtmbfBe81E/likes`;
 
-  // Get data
   static getData = async () => {
     const res = await fetch(API.url);
     const result = await res.json();
     return result;
   };
 
-  // Get Like Data
-  static getLikesCount = async () => {
-    const res = await fetch(API.likesURL);
-    const result = await res.json();
-    return result;
+  static postLikes = async (id) => {
+    const likedMovieID = { item_id: `${id}` };
+    await fetch(API.likesURL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(likedMovieID),
+    });
   };
 }
